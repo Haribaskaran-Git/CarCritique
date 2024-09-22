e50c18d6b55382d8a4e368e766a5dd20df3945c8
Harry <81846401+hariharan1423@users.noreply.github.com>
2024-09-22 17:13:18 +0530

    added search_bar to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../styles/navbar.css'
+import { Link } from 'react-router-dom'
+import { GrSearch } from "react-icons/gr";
+
+
 
 const NavBar = () => {
   return (
-    <div className='navbar'>NavBar</div>
+    <div className='navbar'>
+      <div className="nav-container">
+        <div className="nav-search">
+          <GrSearch></GrSearch>
+          <input type="text" placeholder='Search for brands, models....' />
+        </div>
+        <div className="nav-pages">
+          <Link to="/Home">Home</Link>
+          <Link to="/">Log out</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
