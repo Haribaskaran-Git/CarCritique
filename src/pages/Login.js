47aaf158df84931d71da40b9669d1d4ae2200f3f
Harry <81846401+hariharan1423@users.noreply.github.com>
2024-09-22 16:07:16 +0530

    Complete login page implementation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,38 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function Login() {
-
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate("/Home");
   };
-  
+
   return (
     <div className="login">
       <div className="login-container">
         <div className="login-wrapper">
-          <div className="login-logo">LOGO</div>
+          <div className="login-logo">
+            <img className="logo" src="images/logo.png" alt="Logo" srcset="" />
+          </div>
           <div className="login-form">
-            <form method="post">
-              <label htmlFor="zId">Z-Id: </label>
-              <input type="text" name="zId" id="zId" />
-              <label htmlFor="passwd">Password: </label>
-              <input type="password" name="passwd" id="passwd" />
-              <input type="submit" value="SUBMIT" onClick={handleSubmit} />
+            <form  onSubmit={handleSubmit} method="POST">
+              <div className="zId-input">
+                <label htmlFor="zId" className="label">
+                  Z-Id:<span>*</span>
+                </label>
+                <input type="text" name="zId" id="zId" placeholder="Enter ZId" required/>
+              </div>
+              <div className="passwd-input">
+                <label htmlFor="passwd" className="label">
+                  Password:<span>*</span>
+                </label>
+                <input type="password" name="passwd" id="passwd" placeholder="Password" required/>
+              </div>
+              <div className="btn-input">
+                <input type="submit" value="SIGN IN"/>
+              </div>
             </form>
           </div>
         </div>
