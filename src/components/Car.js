74ff32c6772fca3df91b68439d122e5c8fab9ddd
BASKARAN-NATARAJAN Hari <hari.baskaran-natarajan@rntbci.com>
2024-09-23 16:27:42 +0530

    Added Car Data

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Car = () => {
+  const carPrice = 1000000; 
+  const formattedPrice = `₹${carPrice.toLocaleString()}`;
+
   return (
     <div className='car'>
-      <div className='car-name'>
-        <h1>Toyota</h1>
+      <div className='car-container'>
+        <h1 className='car-name'>RENAULT</h1>
+        
+        <h1 className='car-price'>{formattedPrice}</h1>
+        
+        <img className='carLogo' src="images/car.jpg" alt="Logo" />
       </div>
-    
     </div>
-  )
+  );
 }
 
-export default Car
+export default Car;
